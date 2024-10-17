001156b66e2aca7682abab05ebe39d5242ebd97e
luminosita <milosh@emisia.net>
2024-10-17 20:26:02 +0200

    disable cloud-init output

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "result" {
     value = {
         for k, v in var.images : k => {
             ip = proxmox_virtual_environment_vm.vm[k].ipv4_addresses[1][0]
-            cloud-init = local.cloud-init-data[k]
+            #cloud-init = local.cloud-init-data[k]
         }
     }
 }
